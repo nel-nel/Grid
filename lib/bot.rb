8a6c48cd45f30aacadc1909f0657f5eb013de18d
nel-nel <nvdelcheva@gmail.com>
2018-11-21 11:49:33 +0200

    working solution, without xtest and ytest

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,116 +1,20 @@
-#The rover receives a char array of commands e.g. RMMLM and returns the finishing point after the moves e.g. 2,1,N
+require_relative "../lib/bot"
+require "test/unit"
 
-#You are given the initial starting point (0,0,N)
-
-class Bot
-  #constuctor 
-  attr_accessor :coordinates, :moves, :direction
+class TestBot < Test::Unit::TestCase 
   
-  def initialize (coordinates, moves, obstacles)
-    @obstacles = obstacles 
-    @x = coordinates[0]
-    @y = coordinates[1]
-    @xtest = coordinates[0]
-    @ytest = coordinates[1]
-    @direction = coordinates[2] 
-    @moves = moves
+  def test_basic
+    assert_equal([0,1], Bot.new([0,0,'N'],['M'],[[7,5]]).calculate_position(),"Basic test failed")
   end
-    
-  def calculate_position()
-    for move in @moves
-      self.change_direction(move)
-      if move == 'M'
-      	self.change_position()
-      end #if
-      #check for grid end 
-      check_point() 
-      if self.no_obstacles() == false      
-        return ["Ðž",@x,@y]
-        #break
-      else
-        @x = @xtest
-        @y = @ytest      
-      end #if 
-    end #for
-    return [@xtest, @ytest]
-  end #calculate_position
-    
-  def change_direction(move)    
-    case move 
-    when 'R'
-      case @direction
-      when 'N'
-        @direction = 'E'
-      when 'S'
-        @direction = 'W'
-      when 'E'
-        @direction = 'S'
-      when 'W'
-        @direction = 'N'
-      end       
-      
-    when 'L'
-      case @direction
-      when 'N'
-        @direction = 'W'                 
-      when 'S'
-        @direction = 'E'     
-      when 'E'
-        @direction = 'N'          
-      when 'W'
-        @direction = 'S'     
-      end
-    when 'M'
-      #don't change @direction in that case
-    end
+
+  def test_obstacles
+    assert_equal([2,3], Bot.new([3,3,'N'],['L','M','R','Ì'],[[2,4]]).calculate_position(),"Obstacle test failed")
   end 
-  def change_position()
-    case @direction
-    when 'N'
-        if check_point(@x,@y++)
-            @y += 1
-        end
-    when 'S'
-        if check_point(@x,@y--)
-            @y -= 1
-        end
-    when 'E'
-        if check_point(@x++,@y)
-            @xtest += 1
-        end
-    when 'W'
-        if check_poin(@x--,@y)    
-            @x -= 1
-        end
-    end #case   
-  end #change_position()
-
-  def check_point(x,y)
-    #check for the ends of the grid
-    if x < 0  &&  @direction == 'W' 
-      @x= 9      
-    elsif x > 10  &&  @direction == 'E' 
-      @x=1      
-    elsif y < 0  &&  @direction == 'S'   
-      @y = 9      
-    elsif y > 10  &&  @direction == 'N' 
-      @ytest=1      
-    else
-      return true
-    end #if
-  end #check_point
-
-  def no_obstacles()
-    no_obstacles = true
-    for obstacle in @obstacles
-      if obstacle[0] == @xtest && obstacle[1] == @ytest
-        no_obstacles = false
-        break 
-      else
-        no_obstacles = true 
-      end #if  
-    end #for
-    return no_obstacles
-  end #end of no_obstacles()
-
-end #end of class
+  
+  def test_wrapsArround   
+    assert_equal([9,0], Bot.new([0,0,'N'],['R','L','L','M','L'],[]).calculate_position(),"Wrap arround test failed")
+  end
+   def test_wrapsArroundAndObstacle   
+    assert_equal(["O",0,1], Bot.new([0,0,'N'],['M','M'],[[0,2]]).calculate_position(),"Obstacle and wrap arround test failed")
+  end
+end
--- test/bot_test.rb
+++ test/bot_test.rb
@@ -1,22 +1,134 @@
-require_relative "../lib/bot"
-require "test/unit"
+#The rover receives a char array of commands e.g. RMMLM and returns the finishing point after the moves e.g. 2,1,N
 
-class TestBot < Test::Unit::TestCase 
+#You are given the initial starting point (0,0,N)
+
+class Bot
+  #constuctor 
+  attr_accessor :coordinates, :moves, :direction
   
-  def test_basic
-    assert_equal([2,1], Bot.new([0,0,'N'],['R','M','M','L','M'],[[7,5]]).calculate_position(),"Basic test failed")
+  def initialize (coordinates, moves, obstacles)
+    @obstacles = obstacles 
+    @x = coordinates[0]
+    @y = coordinates[1]
+    @direction = coordinates[2] 
+    @moves = moves
+    @stop = false
   end
-
-  def test_obstacles
-    assert_equal([2,3], Bot.new([3,3,'N'],['L','M','R','М'],[[2,4]]).calculate_position(),"Obstacle test failed")
+    
+  def calculate_position()
+    for move in @moves
+      self.change_direction(move)
+      if move == 'M'
+      	self.change_position()
+      end #if
+      #check for grid end  
+    end #for
+    if @stop ==false
+      return [@x,@y] 
+    else
+      return ["O",@x,@y]
+    end 
+  end #calculate_position
+    
+  def change_direction(move)    
+    case move 
+    when 'R'
+      case @direction
+      when 'N'
+        @direction = 'E'
+      when 'S'
+        @direction = 'W'
+      when 'E'
+        @direction = 'S'
+      when 'W'
+        @direction = 'N'
+      end       
+      
+    when 'L'
+      case @direction
+      when 'N'
+        @direction = 'W'                 
+      when 'S'
+        @direction = 'E'     
+      when 'E'
+        @direction = 'N'          
+      when 'W'
+        @direction = 'S'     
+      end
+    when 'M'
+      #don't change @direction in that case
+    end
   end 
-  
-  def test_wrapsArround   
-    assert_equal([9,0], Bot.new([0,0,'N'],['R','L','L','M','L'],[]).calculate_position(),"Wrap arround test failed")
-  end
-   def test_wrapsArroundAndObstacle   
-    assert_equal([10,8], Bot.new([6,8,'N'],['L','M','M','M','M'],[[2,8]]).calculate_position(),"Obstacle and wrap arround test failed")
-  end
-end
+  def change_position()
+    case @direction
+    when 'N'
+        if check_point(@x,@y+1)
+          if no_obstacles(@x,@y+1)
+            @y += 1
+          else
+            return ["O",@x,@y]
+          end
+        end
+    when 'S'
+        if check_point(@x,@y-1)
+          if no_obstacles(@x,@y-1)
+            @y -= 1
+          else
+            return ["O",@x,@y]
+          end            
+        end
+    when 'E'
+        if check_point(@x+1,@y)
+          if no_obstacles(@x+1,@y)
+            @x += 1
+          else
+            return ["O",@x, @y]
+          end
+        end
+    when 'W'
+        if check_point(@x-1,@y)    
+          if no_obstacles(@x-1,@y)
+            @x -= 1
+          else
+            return ["O",@x, @y]
+          end
+        end
+    end #case   
+  end #change_position()
+
+  def check_point(x,y)
+    #check for the ends of the grid
+    if x < 0  &&  @direction == 'W' 
+      @x=9 
+      false     
+    elsif x > 10  &&  @direction == 'E' 
+      @x=0      
+      false
+    elsif y < 0  &&  @direction == 'S'   
+      @y = 9      
+      false
+    elsif y > 10  &&  @direction == 'N' 
+      @y=0     
+      false 
+    else
+      true
+    end #if
+  end #check_point
+
+  def no_obstacles(x,y)
+    no_obstacles = true
+    for obstacle in @obstacles
+      if obstacle[0] == x && obstacle[1] == y
+        no_obstacles = false
+        @stop = true
+        break 
+      else
+        no_obstacles = true 
+      end #if  
+    end #for
+    return no_obstacles
+  end #end of no_obstacles()
+
+end #end of class
 
 
